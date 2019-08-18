5acfba4b6c77c4f982b38be4c402839949cc062c
MinHeinHtet <40414300@live.napier.ac.uk>
2019-08-18 14:27:41 +0630

    rename appintegration

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -244,11 +244,17 @@ public class App {
         App a = new App();
 
         // Connect to database
-        a.connect("localhost:33060");
+        if (args.length < 1)
+        {
+            a.connect("localhost:3306");
+        }
+        else
+        {
+            a.connect(args[0]);
+        }
 
         Department dept = a.getDepartment("Sales");
         ArrayList<Employee> employees = a.getSalariesByDepartment(dept);
-
         // Print salary report
         a.printSalaries(employees);
 
